dc3510bd840e02914003fe6e4b915c6e14b9f095
Am I Sumon <sumonmujibnagar247@gmail.com>
2018-07-10 22:05:26 -0700

    80.homepage

--- 79.mail-chimp/assets/js/main.js
+++ 79.mail-chimp/assets/js/main.js
@@ -3,7 +3,7 @@
 
     jQuery(document).ready(function($){
     	$(".emailchimp-form").ajaxChimp({
-    		url: 'https://amisumon.us18.list-manage.com/subscribe/post?u=4f11d75c96784d4a6abedd89f&amp;id=737bbd702d'
+    		url: 'https://amisumon.us18.list-manage.com/subscribe/post?u=4f11d75c96784d4a6abedd89f&id=737bbd702d'
     	});
 
 
