f976fba851a0e8271fbf1b9fe6d8941714824991
Am I Sumon <sumonmujibnagar247@gmail.com>
2018-07-15 22:35:32 -0700

    87.owl-carousel

---
+++ 87.owl-carousel/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		$(".travel-area").owlCarousel({
+    		items: 1,
+    		dots: true,
+    		nav: true,
+            navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    		loop: true,
+    	   autoplay: false,
+          
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
