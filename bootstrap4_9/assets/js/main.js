bc7b1dab5fb79b9751d152fd399081ca6ef6e7d2
Am I Sumon <sumonmujibnagar247@gmail.com>
2018-07-19 23:20:21 -0700

    bootstrap4_9

---
+++ bootstrap4_9/assets/js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+  $('.homepage-slide-area').owlCarousel({
+  		items: 1,
+  		loop: true,
+  		nav: false,
+  		dots: true,
+  		autoplay: true,
+
+  });
+  $('.logo-caroduel').owlCarousel({
+  		items: 5,
+  		loop: true,
+  		margin: 30,
+  		nav: false,
+  		dots: true,
+  		autoplay: true,
+
+  });
+
+});
